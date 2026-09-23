946d1a031742dd2519be631823e4fbd69e4bc66e
Maicel Monzón <maicel.monzon@gmail.com>
2026-09-23 10:10:21 -0400

    Rescata configuracion de navegacion y paquetes desde copia local de Havana

--- install_packages.R
+++ install_packages.R
@@ -1,5 +1,6 @@
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 
+# Paquetes requeridos para el sitio web y análisis estadístico en R
 pkgs <- c(
   "knitr",
   "rmarkdown",
@@ -14,8 +15,23 @@ pkgs <- c(
   "janitor",
   "shiny",
   "DT",
-  "plotly"
+  "plotly",
+  "tidyverse",
+  "readxl",
+  "here",
+  "srvyr",
+  "survey",
+  "gt",
+  "gtsummary",
+  "pak"
 )
 
 to_install <- setdiff(pkgs, rownames(installed.packages()))
-if (length(to_install) > 0) install.packages(to_install)
+if (length(to_install) > 0) {
+  install.packages(to_install, dependencies = TRUE)
+}
+
+# Comentario para paquetes de GitHub / especializados (descomentar si es necesario instalar desde fuentes específicas):
+# if (!requireNamespace("pak", quietly = TRUE)) install.packages("pak")
+# if (!requireNamespace("nutriR", quietly = TRUE)) try(pak::pkg_install("nutriR"), silent = TRUE)
+# if (!requireNamespace("SPADE.RIVM", quietly = TRUE)) try(pak::pkg_install("SPADE.RIVM"), silent = TRUE)
